8320bdc250bb6ead65039002af4512f9464df808
“Swapnil <swapnilkumbhar366@gmail.com>
2024-08-09 17:26:06 +0530

    Created Footer Section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import '../Footer/Footer-style.css'
+
+const Footer = () => {
+  
+  
+  return (
+    <>
+     <footer className="footer">
+       <p>Copyright 2024 - Socialbook.com</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
