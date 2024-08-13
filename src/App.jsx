53ad46820e18ef1a1b76d95a02408653326654b9
“Swapnil <swapnilkumbhar366@gmail.com>
2024-08-13 19:54:34 +0530

    Add all components in app.jsx and add some css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
-
+import LeftSideBar from './components/LeftSideBar/LeftSideBar'
+import MainContent from './components/MainContent/MainContent'
+import RightSideBar from './components/RightSideBar/RightSideBar'
 
 function App() {
   
@@ -8,6 +10,11 @@ function App() {
   return (
     <>
       <Navbar /> 
+      <div className="container">
+        <LeftSideBar />
+        <MainContent/>
+        <RightSideBar/>
+      </div>
     </>
   )
 }
